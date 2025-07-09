4d27b4de4ea7c61c8a696c6a99c9e7e201f64ac3
cnusslein <cnusslein99@gmail.com>
2025-07-08 21:47:39 -0400

    updated microstrain operation

--- epicallypowerful/sensing/__init__.py
+++ epicallypowerful/sensing/__init__.py
@@ -1,3 +1,5 @@
 # Import Mangement for epicallypowerful sensing modules
 import epicallypowerful.sensing.imu_data
+import epicallypowerful.sensing.microstrain_imu
 from .imu_data import IMUData
+from .microstrain_imu import MicrostrainImus
